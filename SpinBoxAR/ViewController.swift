0f54d95ff5129ccf2d54c0e1ece05b1750bc9987
Pavel <pkoyushev@gmail.com>
2021-02-10 21:40:33 +0300

    random color box

--- SpinBoxAR/ViewController.swift
+++ SpinBoxAR/ViewController.swift
@@ -14,12 +14,13 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        createBOX()
+        addTapGestureToSceneView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         let config = ARWorldTrackingConfiguration()
+            config.planeDetection = .horizontal
         myCanvas.session.run(config)
     }
     
@@ -28,10 +29,15 @@ class ViewController: UIViewController {
         myCanvas.session.pause()
     }
     
-    func createBOX() {
+    func addTapGestureToSceneView() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.didTap(withGestureRecognizer:)))
+        myCanvas.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    func createBOX(x: Float = 0, y: Float = 0, z: Float = -0.2) {
         let box = SCNBox(width: 0.10, height: 0.10, length: 0.10, chamferRadius: 0)
-        let colors = [UIColor.blue, .cyan, .green, .magenta, .purple, .brown]
-        
+        let colors = [UIColor.random(), UIColor.random(), UIColor.random(), UIColor.random(), UIColor.random(), UIColor.random()]
+        print(UIColor.random())
         let boxMaterials = colors.map({ (colors) -> SCNMaterial in
             let material = SCNMaterial()
                 material.diffuse.contents = colors
@@ -40,14 +46,45 @@ class ViewController: UIViewController {
         )
         box.materials = boxMaterials
         
-        let rotateOnce = SCNAction.rotateBy(x: 0, y: 2*CGFloat.pi, z: 0, duration: 3)
-        let rotateForever = SCNAction.repeatForever(rotateOnce)
+        let randomDuration = Double.random(in: 3...7)
         
+        let rotateOnce = SCNAction.rotateBy(x: 0, y: 2*CGFloat.pi, z: 0, duration: randomDuration)
+        let rotateForever = SCNAction.repeatForever(rotateOnce)
         let boxNode = SCNNode(geometry: box)
-            boxNode.position = SCNVector3(0, 0, -0.5)
+            boxNode.geometry = box
+            boxNode.position = SCNVector3(x, y, z)
             boxNode.runAction(rotateForever)
         
         myCanvas.scene.rootNode.addChildNode(boxNode)
     }
+    
+    @objc func didTap(withGestureRecognizer recognizer: UIGestureRecognizer) {
+        let tapLocation = recognizer.location(in: myCanvas)
+        let hitTestResults = myCanvas.hitTest(tapLocation)
+       
+        guard let node = hitTestResults.first?.node else {
+            let hitTestResultsWithFeaturePoints = myCanvas.hitTest(tapLocation, types: .featurePoint)
+            if let hitTestResultWithFeaturePoints = hitTestResultsWithFeaturePoints.first {
+                let translation = hitTestResultWithFeaturePoints.worldTransform.translation
+                createBOX(x: translation.x, y: translation.y, z: translation.z)
+            }
+            return
+        }
+        node.removeFromParentNode()
+    }
 }
 
+extension float4x4 {
+    var translation: SIMD3<Float> {
+        let translation = self.columns.3
+        return SIMD3(translation.x, translation.y, translation.z)
+    }
+}
+
+extension UIColor {
+    static func random() -> UIColor {
+        return UIColor(red: .random(in: 0...1),
+                       green: .random(in: 0...1),
+                       blue: .random(in: 0...1), alpha: 1.0)
+    }
+}
